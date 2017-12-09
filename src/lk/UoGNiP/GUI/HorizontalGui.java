a514ce2954a08683361fd9d8770c715071aa622d
idl99 <ihandilnath2012@gmail.com>
2017-12-09 19:44:27 +0530

    Merged HorizontalGui and Horizontal Stars, VerticalGui and Vertical Stars

--- src/lk/UoGNiP/GUI/HorizontalGui.java
+++ src/lk/UoGNiP/GUI/HorizontalGui.java
@@ -84,3 +84,24 @@ public class HorizontalGui extends JFrame {
 
     }
 }
+
+class HorizontalStars extends JPanel {
+
+    int numOfStars = 0;
+
+    @Override
+    public void paint(Graphics g) {
+
+        Graphics2D g2d = (Graphics2D) g;
+
+        for(int i=0; i<numOfStars; i++){
+            g2d.setFont(new Font("TimesRoman", Font.BOLD, 20));
+            g2d.drawString("*",20+(i*10),45);
+        }
+    }
+
+    public HorizontalStars(int paramNumOfStars){
+        this.numOfStars = paramNumOfStars;
+    }
+
+}
--- src/lk/UoGNiP/GUI/VerticalGui.java
+++ src/lk/UoGNiP/GUI/VerticalGui.java
@@ -76,3 +76,25 @@ public class VerticalGui extends JFrame{
     }
 
 }
+
+class VerticalStars extends JPanel{
+
+    int numOfStars = 0;
+
+    @Override
+    public void paint(Graphics g) {
+
+        Graphics2D g2d = (Graphics2D) g;
+
+        for(int i=0; i<numOfStars; i++){
+            g2d.setFont(new Font("TimesRoman", Font.BOLD, 20));
+            g2d.drawString("*",10,30+(i*20));
+        }
+    }
+
+    public VerticalStars(int paramNumOfStars){
+        this.numOfStars = paramNumOfStars;
+    }
+
+}
+
