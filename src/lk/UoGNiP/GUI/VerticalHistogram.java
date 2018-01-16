cee5dfa727477a86376255c0eeedffdc6d8465ca
idl99 <ihandilnath2012@gmail.com>
2018-01-16 09:33:15 +0530

    Removed unnecessary comment in paint method of Vertical Histogram

--- src/lk/UoGNiP/GUI/VerticalHistogram.java
+++ src/lk/UoGNiP/GUI/VerticalHistogram.java
@@ -84,15 +84,6 @@ class VerticalStars extends JPanel{
 
     @Override
     public void paint(Graphics g) {
-
-        /*Graphics2D g2d = (Graphics2D) g;
-
-        for(int i=0; i<numOfStars; i++){
-            g2d.setFont(new Font("TimesRoman", Font.BOLD, 20));
-            g2d.drawString("*",10,30+(i*20));
-        }
-
-        */
         Graphics2D g2d = (Graphics2D) g;
 
         int col=0;
